4b6389b8d9c7edf16aad913f52a190b468f63b89
Manish-Kushwaha-5959 <manishkushwaha5959@gmail.com>
2026-07-05 15:26:22 +0530

    chatbot with UI

---
+++ 05_Chatbot/chatbot_backend.py
@@ -0,0 +1,57 @@
+from langgraph.graph import StateGraph, START, END
+from langgraph.checkpoint.memory import InMemorySaver
+from typing import TypedDict, Annotated
+from langgraph.graph.message import add_messages
+from langchain_core.messages import BaseMessage, HumanMessage
+from langchain_huggingface import ChatHuggingFace, HuggingFaceEndpoint
+from dotenv import load_dotenv
+
+load_dotenv()
+
+llm = HuggingFaceEndpoint(
+    repo_id="meta-llama/Llama-3.1-8B-Instruct",
+    task="text-generation"
+)
+
+model = ChatHuggingFace(llm=llm)
+
+class ChatState(TypedDict):
+    messages : Annotated[list[BaseMessage], add_messages]
+
+def chat_node(state: ChatState):
+    messages = state["messages"]
+
+    response = model.invoke(messages)
+
+    return {"messages" : [response]}
+
+# create graph
+graph = StateGraph(ChatState)
+
+# add nodes
+graph.add_node("chat_node", chat_node)
+
+# add edges
+graph.add_edge(START, "chat_node")
+graph.add_edge("chat_node", END)
+
+# checkpoint
+checkpointer = InMemorySaver()
+
+# compile graph
+chatbot = graph.compile(checkpointer=checkpointer)
+
+if __name__ == "__main__":
+
+    config = {"configurable" : {"thread_id" : "1"}}
+    while True:
+        user_input = input("User: ")
+
+        if user_input.strip().lower() in ["exit", "quite", "bye"]:
+            break
+
+        response = chatbot.invoke({"messages": [HumanMessage(content=user_input)]}, config=config)
+
+        print(f"AI: {response['messages'][-1].content}")
+
+
---
+++ 05_Chatbot/chatbot_ui.py
@@ -0,0 +1,25 @@
+import streamlit as st
+from langchain_core.messages import HumanMessage
+from dotenv import load_dotenv
+from chatbot_backend import chatbot
+CONFIG = {"configurable": {"thread_id": "1"}}
+
+load_dotenv()
+if "messages" not in st.session_state:
+    st.session_state["messages"] = []
+
+for message in st.session_state["messages"]:
+    with st.chat_message(message["role"]):
+        st.write(message["content"])
+
+user_input = st.chat_input()
+
+if user_input:
+    st.session_state["messages"].append({"role": "user", "content": user_input})
+    with st.chat_message("user"):
+        st.write(user_input)
+
+    response = chatbot.invoke({"messages": [HumanMessage(content=user_input)]}, config=CONFIG)['messages'][-1]
+    st.session_state["messages"].append({"role": "ai", "content": response.content})
+    with st.chat_message("ai"):
+        st.write(response.content)

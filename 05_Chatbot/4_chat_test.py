a6e1d30ba2b9cc73b9a64af2f47db4cc2a21319c
Manish-Kushwaha-5959 <manishkushwaha5959@gmail.com>
2026-07-08 22:52:48 +0530

    added chat title generation feature to the chatbot

---
+++ 05_Chatbot/4_chat_test.py
@@ -0,0 +1,102 @@
+import streamlit as st
+from langchain_core.messages import HumanMessage
+from chatbot_backend import chatbot
+import uuid
+
+# ************************** utility function *********************************
+def generate_threadid():
+    thread_id = uuid.uuid4()
+    return thread_id
+
+def reset():
+    st.session_state["thread_id"] = generate_threadid()
+    add_threads(st.session_state["thread_id"])
+    st.session_state["messages"] = []
+
+def add_threads(thread_id):
+    st.session_state["threads"].append({"thread_id": thread_id, "title" : str(thread_id)})
+
+def load_conversation(thread_id):
+    return chatbot.get_state({"configurable": {"thread_id": thread_id}}).values['messages']
+
+# *************************** session *******************************************
+
+if "messages" not in st.session_state:
+    st.session_state["messages"] = []
+
+if "thread_id" not in st.session_state:
+    st.session_state["thread_id"] = generate_threadid()
+
+if "threads" not in st.session_state:
+    st.session_state["threads"] = []
+    add_threads(st.session_state["thread_id"])
+
+
+# ************************** sidebar *******************************************
+st.sidebar.title("Langgraph Chatbot")
+
+if st.sidebar.button("New Chat"):
+    reset()
+
+st.sidebar.header("My Conversations")
+for thread in st.session_state["threads"][::-1]:
+    if st.sidebar.button(thread["title"]):
+        messages = load_conversation(thread["thread_id"])
+        st.session_state["thread_id"] = thread["thread_id"]
+        temp_messages = []
+        for msg in messages:
+            if isinstance(msg, HumanMessage):
+                role = "user"
+            else:
+                role = 'ai'
+            temp_messages.append({'role': role, 'content': msg.content})
+        st.session_state["messages"] = temp_messages
+
+
+# ****************************** UI ***********************************************
+
+for message in st.session_state["messages"]:
+    with st.chat_message(message["role"]):
+        st.write(message["content"])
+
+user_input = st.chat_input("Type here")
+CONFIG = {"configurable": {"thread_id": st.session_state["thread_id"]}}
+NEW_CHAT_CONFIG = {"configurable": {"thread_id": "new_chat_thread"}}
+if user_input:
+    st.session_state["messages"].append({"role": "user", "content": user_input})
+
+    if len(st.session_state["messages"]) == 1:
+        prompt = f"""
+Based only on the user's first message, generate a concise chat title.
+
+user first message: {user_input}
+
+Rules:
+- Maximum 2–5 words.
+- Capture the main topic or intent.
+- Use natural title case.
+- Do not include quotation marks, emojis, punctuation at the end, or prefixes like "Title:".
+- Do not invent details that aren't in the user's message.
+- If the message is a greeting (e.g., "Hi", "Hello", "How are you"), return "General Chat".
+- Output only the title and nothing else.
+"""
+        title = chatbot.invoke({"messages": HumanMessage(content=prompt)}, config=NEW_CHAT_CONFIG)["messages"][-1].content
+        for thread in st.session_state["threads"]:
+            if thread["thread_id"] == st.session_state["thread_id"]:
+                thread["title"] = title
+                break
+
+    with st.chat_message("user"):
+        st.write(user_input)
+    
+    with st.chat_message("ai"):
+        response = st.write_stream(
+            message_chunk.content for message_chunk, metadata in chatbot.stream(
+                {"messages": [HumanMessage(content=user_input)]},
+                config=CONFIG,
+                stream_mode="messages"
+            )
+        )
+    st.session_state["messages"].append({"role": "ai", "content": response})
+    st.rerun()
+    
